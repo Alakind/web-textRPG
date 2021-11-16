72b97500d5e5a232e7eb2f68dd5d0e1bc6e01b79
Alex Ivakin <sash.ivakin@yandex.ru>
2021-11-16 09:48:23 +0700

    rename fix

--- frontend/src/pages/game/panels/index.js
+++ frontend/src/pages/game/panels/index.js
@@ -2,7 +2,7 @@ import { InputPanel } from './inputPanel/InputPanel';
 import { CharacterPanel } from './characterPanel/CharacterPanel';
 import { StatusPanel } from './statusPanel/StatusPanel';
 import { JournalPanel } from './journalPanel/JournalPanel';
-import { SwitchPanel } from './switchPanel/PanelSwitch';
+import { SwitchPanel } from './switchPanel/SwitchPanel';
 
 export {
   InputPanel,
