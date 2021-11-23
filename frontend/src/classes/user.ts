45a2c1982cd4b8f5106309b68d11cb3d441e77c8
Alex Ivakin <sash.ivakin@yandex.ru>
2021-11-23 09:20:22 +0700

    fix

--- frontend/src/classes/user.ts
+++ frontend/src/classes/user.ts
@@ -2,11 +2,8 @@ import { UserOptions } from './options';
 
 export class User {
     private email: string;
-
     private username: string;
-
     private password: string;
-    
     private id: number;
 
     constructor({ email, username, password, id }: UserOptions) {
