38835137da29d4063b323c230293cb0d73e50f66
Alex Ivakin <sash.ivakin@yandex.ru>
2021-11-30 09:54:21 +0700

    health and mana bar

---
+++ frontend/src/pages/game/components/common/bars/HealthBar.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../../../../../styles/common/bars.css';
+
+interface Props {
+    health: number,
+    maxHealth: number,
+}
+
+export function HealthBar({ health, maxHealth }: Props) {
+  return (
+    <svg className="bar">
+      <rect className="healthBarOuter" />
+      <rect className="healthBarInner" width={`${(health / maxHealth) * 100}%`} />
+    </svg>
+  );
+}
---
+++ frontend/src/pages/game/components/common/bars/ManaBar.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../../../../../styles/common/bars.css';
+
+interface Props {
+    mana: number,
+    maxMana: number,
+}
+
+export function HealthBar({ mana, maxMana }: Props) {
+  return (
+    <svg className="bar">
+      <rect className="manaBarOuter" />
+      <rect className="manaBarInner" width={`${(mana / maxMana) * 100}%`} />
+    </svg>
+  );
+}
