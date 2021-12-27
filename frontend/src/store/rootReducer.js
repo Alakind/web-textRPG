f9df26368f650d54818a87572c0b2abaae1dd0bb
Alakind <sash.ivakin@yandex.ru>
2021-12-27 23:22:03 +0700

    decisions reducer to riit reducer

--- frontend/src/store/rootReducer.js
+++ frontend/src/store/rootReducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import { authReducer } from '../ducks/auth';
 import { displayReducer } from '../ducks/display';
 import { characterReducer } from '../ducks/character';
+import { decisionsReducer } from '../ducks/decisions';
 
 export default function createRootReducer() {
   return combineReducers({
     auth: authReducer,
     display: displayReducer,
     character: characterReducer,
+    decisions: decisionsReducer,
   });
 }
