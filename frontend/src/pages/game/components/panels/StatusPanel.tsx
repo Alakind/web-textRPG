c5089d88bf2f29e36be02f781bca093998d3605e
Alex Ivakin <sash.ivakin@yandex.ru>
2021-12-27 14:22:31 +0700

    new map

--- frontend/src/pages/game/components/panels/StatusPanel.tsx
+++ frontend/src/pages/game/components/panels/StatusPanel.tsx
@@ -5,7 +5,7 @@ export function StatusPanel() {
   return (
     <div className="topPanel">
       <h3>Status panel</h3>
-      <img width="40%" src="./map.jpeg" alt="map" />
+      <img width="55%" src="./map.jpeg" alt="map" />
     </div>
   );
 }
