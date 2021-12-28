72a3f01f36b1f0a9a6335e8cd6061ff5cb18fb7c
Alex Ivakin <sash.ivakin@yandex.ru>
2021-12-28 11:29:52 +0700

    locations mocks

---
+++ frontend/src/util/locationsMock.ts
@@ -0,0 +1,16 @@
+export const locationsMock = {
+  city: {
+    location: 'city',
+    options: [
+      { text: 'Leave the city', description: 'You are travelling to the city', destination: 'city' },
+      { text: 'Set a camp', description: 'You are making a campfire and resting the night' },
+    ],
+  },
+  road: {
+    location: 'road',
+    options: [
+      { text: 'Travel to the city', description: 'You are now leaving the city', destination: 'road' },
+      { text: 'Rest at the tavern', description: 'You are taking a good room at local tavern' },
+    ],
+  },
+};
